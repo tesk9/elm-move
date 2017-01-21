4da87f3451ac196eeb0c1e01f7e5b4ccb7f207f7
Tessa Kelly <tessakkelly@gmail.com>
2017-01-20 17:54:51 -0800

    Build a module for moving elm files

--- move.js
+++ move.js
@@ -6,10 +6,6 @@ var fs = require("fs");
 var projectPath = process.argv[2];
 var destination = process.argv[3];
 
-
-validatePaths([projectPath, destination]);
-moveProject();
-
 function validatePaths(paths) {
   paths.forEach(function(path) {
     if (!fs.existsSync(path)) {
@@ -18,54 +14,58 @@ function validatePaths(paths) {
   });
 }
 
-function moveProject() {
-  fs.readdir(projectPath, function(err, files) {
+function moveProject(original, destination) {
+  validatePaths([original, destination]);
+
+  fs.readdir(original, function(err, files) {
     var elmFileNames = files.filter(function(file) {
-      return /.elm$/.test(file)
+      return /.elm$/.test(file);
     });
 
     if (!elmFileNames.length) {
       console.log("No Elm files found.");
     }
-    elmFileNames.forEach(moveElmFile);
+
+    elmFileNames.forEach(moveElmFile(original, destination));
   });
 }
 
-function moveElmFile(elmFileName, _index, elmFileNames) {
-  var originalFilePath = path.join(projectPath, elmFileName);
-  var newFilePath = path.join(destination, elmFileName);
-
-  var copyAndModifyElmFile = createNewElmFile(originalFilePath, newFilePath, elmFileNames);
-  fs.readFile(originalFilePath, 'utf8', copyAndModifyElmFile);
-}
-
-function createNewElmFile(originalFilePath, newFilePath, elmFileNames) {
-  return function(err, fileContents) {
-    if (err) { throw err; }
-    var newFileContents = replaceModuleNames(fileContents, elmFileNames);
-    fs.writeFile(newFilePath, newFileContents, function(err) {
+function moveElmFile(projectPath, destination) {
+  var createNewElmFile = function(originalFilePath, newFilePath, elmFileNames) {
+    return function(err, fileContents) {
       if (err) { throw err; }
-      logMove(originalFilePath, newFilePath);
-      removeFile(originalFilePath)
-    });
+      var newFileContents = replaceModuleNames(fileContents, elmFileNames);
+      fs.writeFile(newFilePath, newFileContents, function(err) {
+        if (err) { throw err; }
+        logMove(originalFilePath, newFilePath);
+        removeFile(originalFilePath)
+      });
+    }
+  };
+
+  var replaceModuleNames = function(fileContents, elmFileNames) {
+    return elmFileNames.reduce(function(file, elmFileName) {
+      var oldModuleName = getModuleName(projectPath, elmFileName);
+      var newModuleName = getModuleName(destination, elmFileName);
+
+      var re = new RegExp(oldModuleName)
+      return file.replace(oldModuleName, newModuleName)
+    }, fileContents);
+  };
+
+
+  return function(elmFileName, _index, elmFileNames) {
+    var originalFilePath = path.join(projectPath, elmFileName);
+    var newFilePath = path.join(destination, elmFileName);
+
+    fs.readFile(originalFilePath, 'utf8', createNewElmFile(originalFilePath, newFilePath, elmFileNames));
   }
 }
 
-function replaceModuleNames(fileContents, elmFileNames) {
-  return elmFileNames.reduce(function(file, elmFileName) {
-    var oldModuleName = getModuleName(projectPath, elmFileName);
-    var newModuleName = getModuleName(destination, elmFileName);
-
-    var re = new RegExp(oldModuleName)
-    return file.replace(oldModuleName, newModuleName)
-  }, fileContents);
-}
-
-
-function getModuleName(project, elmFileName) {
+var getModuleName = function(project, elmFileName) {
   var re = /([A-Z]{1}[A-Za-z]*\/)*[A-Z]{1}[A-Za-z]*/g;
   return path.join(project, elmFileName).match(re)[0].replace(/\//g, ".");
-}
+};
 
 function logMove(originalFilePath, newFilePath) {
   console.log("\nContents Moved:");
@@ -77,3 +77,5 @@ function removeFile(path) {
     if (err) { console.log("ERR", err); }
   });
 }
+
+moveProject(projectPath, destination);
