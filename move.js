278052ac50e731cf014f89bc3138a0d2a99f690b
Tessa Kelly <tessakkelly@gmail.com>
2017-01-20 16:56:19 -0800

    Update the module names to match the path

--- move.js
+++ move.js
@@ -32,17 +32,38 @@ function moveProject() {
   });
 }
 
-function moveElmFile(elmFileName) {
+function moveElmFile(elmFileName, _index, elmFileNames) {
   var originalFilePath = path.join(projectPath, elmFileName);
   var newFilePath = path.join(destination, elmFileName);
-  fs.readFile(originalFilePath, function(err, fileContents) {
+
+  fs.readFile(originalFilePath, 'utf8', function(err, fileContents) {
     if (err) { throw err; }
-    fs.writeFile(newFilePath, fileContents, function(err) {
+
+    var newFileContents = replaceModuleNames(fileContents, elmFileNames);
+
+    fs.writeFile(newFilePath, newFileContents, function(err) {
       if (err) { throw err; }
-      console.log(originalFilePath, "contents copied to", newFilePath);
+      console.log(originalFilePath, "contents moved to", newFilePath);
+
       fs.unlink(originalFilePath, function(err) {
         if (err) { console.log("ERR", err); }
       });
     });
   });
+}
+
+function replaceModuleNames(fileContents, elmFileNames) {
+  return elmFileNames.reduce(function(file, elmFileName) {
+    var oldModuleName = getModuleName(projectPath, elmFileName);
+    var newModuleName = getModuleName(destination, elmFileName);
+
+    var re = new RegExp(oldModuleName)
+    console.log(re)
+    return file.replace(oldModuleName, newModuleName)
+  }, fileContents);
+}
+
+function getModuleName(project, elmFileName) {
+  var re = /([A-Z]{1}[a-z]*\/)*[A-Z]{1}[a-z]*/g;
+  return path.join(project, elmFileName).match(re)[0].replace(/\//g, ".");
 }
